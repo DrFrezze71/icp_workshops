6ae6e851de254fda3fb839281e2490096722242a
Jakub Kozdra <pixel633@gmail.com>
2025-03-13 17:44:37 +0000

    feat: response, input & fontend

--- src/icp_workshops_backend/src/lib.rs
+++ src/icp_workshops_backend/src/lib.rs
@@ -3,37 +3,42 @@ use ic_cdk::api::management_canister::http_request::http_request;
 use ic_cdk::api::management_canister::http_request::HttpMethod;
 use ic_cdk::api::management_canister::http_request::HttpHeader;
 use ic_cdk::println;
+use std::fmt::format;
 
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
 
-#[derive(Debug, serde::Deserialize)] 
+#[derive(Debug, serde::Deserialize)]
 pub struct Response {
     translation_text: String,
 }
 
 #[ic_cdk::update]
-async fn translate() -> String {
+async fn translate(text: String ) -> Result<String, String> {
+    let token = "";
     let arg = CanisterHttpRequestArgument {
         url: "https://api-inference.huggingface.co/models/google-t5/t5-base".to_string(),
         max_response_bytes: None,
-        method: HttpMethod::GET,
-        headers: vec![HttpHeader {
-            name: "Authorization".to_string(),
-            // value: "Bearer API-KEY".to_string(),
-        }],
-        body: Some(r#"{"inputs": "Whats up?"}"#.into()),
+        method: HttpMethod::POST,
+        headers: vec![
+            HttpHeader {
+                name: "Authorization".to_string(),
+                value: format!("Bearer {}", token).to_string(),
+            }
+        ],
+        body: Some(format!(r#"{{"inputs": "{}"}}"#, text).into()),
         transform: None,
     };
 
-    let res = http_request(arg, 100_000_000_000).await.unwrap();
+    let res = http_request(arg, (1_603_113_200 + text.len() * 400).try_into().unwrap())
+        .await
+        .map_err(|error| format!("error while querying data. Status: {:?}, Error: {}", error.0, error.1))?;
 
-    println!("{:?}", res);
-    println!("{:?}", String::from_utf8(res.0.body.clone()));
+    let formatted_res: (Response, ) = serde_json::from_slice(&res.0.body)
+        .map_err(|error| format!("error while parsing response. Error: {}", error))?;
 
-    let formated_res: Response = serde_json::from_slice(&res.0.body).unwrap();  
-
-    String::new()
+    
+    Ok(formatted_res.0.translation_text)
 }
